68bce17f8f4d142ae7004ed07d8e15554677fd69
githron <allewnor@gmail.com>
2023-02-07 08:13:47 +0000

    Toggle Switch

--- js change image at screen size/app.js
+++ js change image at screen size/app.js
@@ -1,14 +1,31 @@
 // const image = document.querySelector("img");
-const image = document.getElementById("tImgX");
+// const image = document.getElementById("tImgX");
 
-function updateImageSource() {
-    if (window.matchMedia("(max-width: 730px)").matches) {
-        image.src = "assets/images/image-web-3-mobile.jpg";
-    } else {
-        image.src = "assets/images/image-web-3-desktop.jpg";
+// function updateImageSource() {
+//     if (window.matchMedia("(max-width: 730px)").matches) {
+//         image.src = "assets/images/image-web-3-mobile.jpg";
+//     } else {
+//         image.src = "assets/images/image-web-3-desktop.jpg";
+//     }
+// }
+
+// window.addEventListener("resize", updateImageSource);
+
+// updateImageSource();
+
+$(document).ready(function () {
+    const image = $("#tImgX");
+
+    function updateImageSource() {
+        if ($(window).width() <= 919) {
+            image.attr("src", "assets/images/image-web-3-mobile.jpg");
+        } else {
+            image.attr("src", "assets/images/image-web-3-desktop.jpg");
+            $(".menuBox").removeClass("open");
+        }
     }
-}
 
-window.addEventListener("resize", updateImageSource);
+    $(window).resize(updateImageSource);
 
-updateImageSource();
+    updateImageSource();
+});
