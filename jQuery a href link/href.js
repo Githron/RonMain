4c12856782ccc3290ab4217446bf76b55397afe3
githron <allewnor@gmail.com>
2023-02-08 01:20:14 +0000

    init

---
+++ jQuery a href link/href.js
@@ -0,0 +1,12 @@
+//THIS IS JQUERY div id = link anchor hrrf function
+$(document).ready(function () {
+    $("#link").click(function () {
+        window.location.href =
+            "https://githron.github.io/romelolacida.com/YT%20Clone/";
+    });
+
+    $("#index2, #stepTo2").click(function () {
+        window.location.href = "index2.html";
+    });
+});
+//end
